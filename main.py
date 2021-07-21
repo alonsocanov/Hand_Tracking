612ac381a244d036c881201e9ca94ec9b763fe94
alonsocanov <alonsocanov@outlook.com>
2021-07-21 15:41:54 -0500

    Added model to real time video feed

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import time
 import utils
 from hand_tracking import HandTracking
+import argparse
+import torch
+import numpy as np
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--count_alpha", type=str,
+                        help="choose [count, alpha]", default='alpha')
+    parser.add_argument("--model_path", type=str,
+                        help="Path to model", default='data/model.pth')
+    args = parser.parse_args()
+
+    if args.count_alpha == 'alpha':
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        model = torch.load(args.model_path)
+        model.eval()
+        model.to(device)
+
     cap = utils.video_capture(0)
     width, height = cap.get(3), cap.get(4)
 
@@ -21,14 +37,26 @@ def main():
         frame = utils.resize(frame, dim)
 
         hands_landamaks = ht.landmark_positions(frame)
-
         if hands_landamaks:
             landmarks = ht.get_all_landmarks(hands_landamaks, dim)
             ht.draw_landmarks(frame, hands_landamaks)
-            count = ht.count_fingers(landmarks)
-            normalized = ht.normalize(landmarks)
-            print(normalized)
-            utils.draw_text(frame, count)
+            if args.count_alpha == 'count':
+                count = ht.count_fingers(landmarks)
+                utils.draw_text(frame, count)
+            elif args.count_alpha == 'alpha':
+                normalized = ht.normalize(landmarks)
+
+                distance = np.asarray(normalized)
+                distance = np.squeeze(distance)
+                if len(distance.shape) == 2:
+                    distance = distance[:, 1]
+                    distance = distance.astype(float).reshape(1, -1)
+                    distance = torch.from_numpy(distance)
+                    distance.to(device)
+
+                    y_hat = model(distance)
+                    y_max = torch.argmax(y_hat, dim=1)
+                    utils.draw_text(frame, int(y_max))
 
         utils.show_img(win_name, frame)
         q_flag = utils.check('q')
