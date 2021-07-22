4289a131c3fe171853e70fc6c8a10c9c2224e892
alonsocanov <alonsocanov@outlook.com>
2021-07-22 11:11:12 -0500

    Added more classes and started taking landmarks

--- take_images.py
+++ take_images.py
@@ -16,7 +16,8 @@ def main():
     q_flag = False
 
     file = 'data/landmarks.csv'
-    labels = {'a': 0, 'b': 1, 'c': 2}
+    labels = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17,
+              's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25, '0': 26, '1': 27, '2': 28, '3': 29, '4': 30, '5': 31, '6': 32, '7': 33, '8': 34, '9': 35}
     keys = list(labels.keys())
 
     while not q_flag:
@@ -35,12 +36,10 @@ def main():
             for key in keys:
                 if utils.check(key):
                     utils.cvs(file, labels[key], normalized)
-            # if utils.check('d'):
-            #     utils.delete_last_entry(file)
-            # utils.draw_text(frame, count)
+                    print('Saved Distances for:', key, ':', labels[key])
 
         utils.show_img(win_name, frame)
-        q_flag = utils.check('q')
+        q_flag = utils.check('-')
 
     utils.destroy_windows()
 
